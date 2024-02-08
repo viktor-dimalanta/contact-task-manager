cbe67a862926cc9cd18c47a1c82a0b4fa8111c8c
Viktor Dimalanta <dimalantaviktorangelo@gmail.com>
2024-02-08 13:55:14 +0800

    fix view auth route

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,8 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-//Route::middleware(['auth'])->group(function () {
     Route::get('/', function () {
-        return view('welcome');
+        return view('auth.login');
     });
 
     Route::get('/dashboard', function () {
@@ -28,7 +27,6 @@ use Illuminate\Support\Facades\Route;
     })->middleware(['auth', 'verified'])->name('dashboard');
 
     Route::middleware('auth')->group(function () {
-        //Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
         Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
@@ -45,5 +43,4 @@ use Illuminate\Support\Facades\Route;
         'index' => 'tasks.index',
         // Add more names for other resourceful routes if needed
     ]);
-//});
 require __DIR__.'/auth.php';
