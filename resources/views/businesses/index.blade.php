5d9da51342cc1b1f1c66195f2362e2b46468d0e7
Viktor Dimalanta <dimalantaviktorangelo@gmail.com>
2024-02-09 14:45:54 +0800

    add design for indexes view

--- resources/views/businesses/index.blade.php
+++ resources/views/businesses/index.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('People') }}
+            {{ __('Business') }}
         </h2>
     </x-slot>
     <div class="my-6">
--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -13,7 +13,7 @@
                     <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                         <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                             <tr>
-                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" class="px-6 py-3">Name</th>
+                                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" class="px-6 py-3">Category Name</th>
                                 <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white" class="px-6 py-3">Actions</th>
                             </tr>
                         </thead>
