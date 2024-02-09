9153c5e3b139e7026adb247973a34fa6abab9963
Viktor Dimalanta <dimalantaviktorangelo@gmail.com>
2024-02-09 11:00:11 +0800

    Add button design

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -24,12 +24,7 @@
                                 <td class="px-6 py-4">{{ $task->for }}</td>
                                 <td class="px-6 py-4">{{ $task->status }}</td>
                                 <td class="px-6 py-4">
-                                    <a href="{{ route('tasks.edit', $task->id) }}" class="text-blue-500 hover:text-blue-700">Edit</a>
-                                    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-500 hover:text-red-700 ml-2">Delete</button>
-                                    </form>
+                                    <a href="{{ route('tasks.edit', $task->id) }}" class="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-green-600 dark:hover:bg-green-700 focus:outline-none dark:focus:ring-green-800">Complete</a>
                                 </td>
                             </tr>
                         @endforeach
@@ -59,12 +54,7 @@
                                 <td class="px-6 py-4">{{ $task->for }}</td>
                                 <td class="px-6 py-4">{{ $task->status }}</td>
                                 <td class="px-6 py-4">
-                                    <a href="{{ route('tasks.edit', $task->id) }}" class="text-blue-500 hover:text-blue-700">Edit</a>
-                                    <form action="{{ route('tasks.destroy', $task->id) }}" method="POST" class="inline">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-500 hover:text-red-700 ml-2">Delete</button>
-                                    </form>
+                                    <a href="{{ route('tasks.edit', $task->id) }}" class="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">Re-Open</a>
                                 </td>
                             </tr>
                         @endforeach
