b27d49f4d3bac035892266f7d8e7282f960493f0
Viktor Dimalanta <dimalantaviktorangelo@gmail.com>
2024-02-10 13:21:20 +0800

    adjust the migration files

--- database/migrations/2024_02_08_022724_create_businesses_table.php
+++ database/migrations/2024_02_08_022724_create_businesses_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('businesses', function (Blueprint $table) {
             $table->id();
             $table->string('business_name');
+            $table->string('business_id');
             $table->string('email');
             $table->string('categories');
             $table->string('tags');
--- database/migrations/2024_02_08_022725_create_people_table.php
+++ database/migrations/2024_02_08_022725_create_people_table.php
@@ -13,7 +13,8 @@ return new class extends Migration
     {
         Schema::create('people', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email');
             $table->string('phone');
             $table->string('business');
